9bf8d033c129d5b06d723a920a59feb109d1ec67
Rafael Medrano <re.medrano@uniandes.edu.co>
2018-12-12 12:00:32 -0500

    Modificaciones generales

--- parcial/static/resemblejs/app.js
+++ parcial/static/resemblejs/app.js
@@ -1,69 +1,109 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
 
-    var fs = require('fs');
+function comparacionImagenes(pathInicio, pathFin , nombreFinal) {
+
     var resemble = require('node-resemble-js');
 
-    const resultadosCalabash = '../../../../calabash/resultados/';
+    var fs = require('fs');
+    var img1 = fs.readFileSync(pathInicio );
+    var img2 = fs.readFileSync(pathFin);
 
-    fs.readdirSync(resultadosCalabash).forEach(file => {
-        var esDirectorio = fs.statSync(resultadosCalabash + file).isDirectory();
-        if (esDirectorio) {
-            var reporte = fs.readFileSync(file + '/reports.html'); //Capturar el reporte HTML
-            console.log(reporte);
-        }
-    })
+    console.log(pathInicio);
+    console.log(pathFin);
+    console.log(nombreFinal);
 
-    /*var api = resemble("buscarMateria.js/materia1.png").onComplete(function(data) {
-        console.log(data);
-    });*/
 
-    /*var img1 = fs.readFileSync('paleta/paleta1.png');
-    var img2 = fs.readFileSync('paleta/paleta2.png');
-
-    resemble(img1).compareTo(img2).ignoreNothing().onComplete(function (data) {
-        data.getDiffImage().pack().pipe(fs.createWriteStream( '../screenshots_resultados/diff_paleta11.png'));
-        console.log(data);
+    var comparacion = resemble(img1).compareTo(img2).onComplete(function (data) {
+        data.getDiffImage().pack().pipe(fs.createWriteStream(nombreFinal));
+        console.log("Comaparacion realizada");
     });
 
-    var img1 = fs.readFileSync('paleta/paleta3.png');
-    var img2 = fs.readFileSync('paleta/paleta4.png');
-
-    resemble(img1).compareTo(img2).ignoreNothing().onComplete(function (data) {
-        data.getDiffImage().pack().pipe(fs.createWriteStream( '../screenshots_resultados/diff_paleta22.png'));
-        console.log(data);
-    });*/
 
 
-//Ejecucion de comparacion buscarMateria.js
-    /*var img1 = fs.readFileSync('buscarMateria.js/materia1.png');
-    var img2 = fs.readFileSync('buscarMateria.js/materia2.png');
-    resemble(img1).compareTo(img2).ignoreNothing().onComplete(function (data) {
-        data.getDiffImage().pack().pipe(fs.createWriteStream( '../screenshots_resultados/diff_materia.png'));
-        console.log(data);
-    });
-    //Ejecucion de comparacion buscarProfesor.js
-    var img1 = fs.readFileSync('buscarProfesor.js/profesor1.png');
-    var img2 = fs.readFileSync('buscarProfesor.js/profesor2.png');
-    resemble(img1).compareTo(img2).ignoreNothing().onComplete(function (data) {
-        data.getDiffImage().pack().pipe(fs.createWriteStream( '../screenshots_resultados/diff_profesor.png'));
-        console.log(data);
-    });
-    //Ejecucion de login.js
-    var img1 = fs.readFileSync('login.js/login1.png');
-    var img2 = fs.readFileSync('login.js/login2.png');
-    resemble(img1).compareTo(img2).ignoreNothing().onComplete(function (data) {
-        data.getDiffImage().pack().pipe(fs.createWriteStream( '../screenshots_resultados/diff_login.png'));
-        console.log(data);
-    });*/
-//res.send( api );
+}
 
-    res.send("Se ejecuron todas las pruebas por favor verifique la carpeta de resultados -screenshots_resultados-");
-
-});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+
+
+    var fs = require('fs');
+    var path = require('path')
+    const resultadosCalabash = '../../../../calabash/resultados_small/';
+    const resultadosCalabashDiff = '../../../../vrt/screenshots_resultados/'
+    var directorios = fs.readdirSync(resultadosCalabash);
+    var directoriosComparar = fs.readdirSync(resultadosCalabash);
+    var comparados = [];
+
+    var cantidadTotal = 0;
+    directorios.forEach(directorio => {
+
+
+        var rutaInicial = resultadosCalabash + directorio
+        var primerArchivoComparar = null;
+
+        fs.readdirSync(rutaInicial).forEach(primerArchivo => {
+            var ext = path.extname(primerArchivo);
+            if ((ext !== '.html')) { //Comparar imagenes
+                primerArchivoComparar = primerArchivo;
+                var arreglo = primerArchivoComparar.split("_")[1];
+                var numero = arreglo.split(".")[0];
+
+                directoriosComparar.forEach(directoriosComparar => {
+
+                    if (!comparados.includes(directoriosComparar)) {
+                        if (directoriosComparar === directorio) {
+                            return true;
+                        } else {
+                            var ruta = resultadosCalabash + directoriosComparar
+                            fs.readdirSync(ruta).forEach(fileFinal => {
+                                if (path.extname(ruta + "/" + fileFinal) !== '.html') {
+                                    if (fileFinal === "screenshot_" + numero + ".png") {
+
+                                        var dir = resultadosCalabashDiff + directorio
+                                        //var img1 = fs.readFileSync(ruta + "/" + fileFinal);
+                                        //var img2 = fs.readFileSync(rutaInicial + "/" + primerArchivoComparar);
+
+                                        if (!fs.existsSync(dir)) {
+                                            fs.mkdirSync(dir);
+                                        }
+
+                                        const options = {
+                                            misMatchPercentage: 100, // %
+                                            isSameDimensions: true, // or false
+                                            dimensionDifference: {width: 0, height: -1}, // defined if dimensions are not the same
+                                        };
+
+                                        //console.log(  dir+ "/"+fileFinal )
+
+                                        comparacionImagenes( ruta + "/" + fileFinal , rutaInicial + "/" + primerArchivoComparar, dir+ "/"+fileFinal );
+                                        //cantidadTotal ++; //comparacionImagenes('diff_paleta' + numero + '.png');
+
+
+                                    }
+                                    else {
+                                        return true;
+                                    }
+                                }
+                                else {
+                                    return true;
+                                }
+                            })
+                        }
+                    }
+                });
+            } else {
+                return true;
+            }
+        });
+
+        comparados.push(directorio);
+
+    })
+
+    for (var i = 0; i < cantidadTotal; i++) {
+        comparacionImagenes('diff_paleta' + i + '.png');
+    }
 });
